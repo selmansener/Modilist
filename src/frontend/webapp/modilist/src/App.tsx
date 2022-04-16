c25bf1c73d3a41668220d98dbd60402fb1aeeb7b
Selman Şener <selman.sener@oplog.com.tr>
2022-04-17 01:42:58 +0300

    add unified auth page

--- src/frontend/webapp/modilist/src/App.tsx
+++ src/frontend/webapp/modilist/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useIsAuthenticated } from "@azure/msal-react";
 import Dashboard from './layouts/dashboard/DashboardLayout';
+import Unauthenticated from './layouts/unauthenticated/UnauthenticatedLayout';
 
 function App() {
+  const isAuthenticated = useIsAuthenticated();
+
   return (
     <div className="App">
-      <Dashboard />
+      <AuthenticatedTemplate>
+        <Dashboard />
+      </AuthenticatedTemplate>
+
+      <UnauthenticatedTemplate>
+        <Unauthenticated />
+      </UnauthenticatedTemplate>
+
     </div>
   );
 }
