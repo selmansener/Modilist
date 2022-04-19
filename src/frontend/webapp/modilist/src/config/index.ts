798de07a82f2522aa08b4e0146389bf80e7ba9b6
Selman Şener <selman.sener@oplog.com.tr>
2022-04-20 02:11:34 +0300

    fix environment issue

--- src/frontend/webapp/modilist/src/config/index.ts
+++ src/frontend/webapp/modilist/src/config/index.ts
@@ -1,7 +1,4 @@
-
-const env = process.env.REACT_APP_ENV === undefined ? "development" : process.env.REACT_APP_ENV;
-
-console.log(env);
+const env = process.env.NODE_ENV === undefined ? "development" : process.env.NODE_ENV;
 
 const envConfig = require(`./config.${env}`).config;
 const msalConfig = require(`./auth/msalConfig.${env}`).msalConfig;
@@ -10,8 +7,6 @@ const loginRequest = require(`./auth/msalConfig.${env}`).loginRequest;
 
 export const config = {
     isDev: env === "development",
-    isInt: env === "int",
-    isStaging: env === "staging",
     isProduction: env === "production",
     ...envConfig,
     ...msalConfig,
