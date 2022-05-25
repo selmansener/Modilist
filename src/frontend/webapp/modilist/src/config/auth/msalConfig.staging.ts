cac2e3a79680c3f82afd3fdb060dd01b6a3de633
Ahmet Selman Şener <selman.sener@modilist.com>
2022-05-25 14:56:42 +0000

    Merged PR 17: Configure environments
    configure environments

--- src/frontend/webapp/modilist/src/config/auth/msalConfig.staging.ts
+++ src/frontend/webapp/modilist/src/config/auth/msalConfig.staging.ts
@@ -6,7 +6,7 @@ export const apiConfig = {
         "https://modilistauth.onmicrosoft.com/c2a43089-5855-4fa3-a46c-41fa67ac0ae4/Accounts.Update",
         "https://modilistauth.onmicrosoft.com/c2a43089-5855-4fa3-a46c-41fa67ac0ae4/Accounts.Get",
     ],
-    webApi: "https://app-modilist-prod-westeu.azurewebsites.net"
+    webApi: "https://app-modilist-staging-westeu.azurewebsites.net"
 };
 
 export const b2cPolicies = {
@@ -39,8 +39,8 @@ export const msalConfig = {
         clientId: "c2a43089-5855-4fa3-a46c-41fa67ac0ae4", // This is the ONLY mandatory field; everything else is optional.
         authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
         knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-        redirectUri: "https://app.modilist.com", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
-        postLogoutRedirectUri: "https://app.modilist.com",
+        redirectUri: "https://staging.modilist.com", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+        postLogoutRedirectUri: "https://staging.modilist.com",
         navigateToLoginRequestUrl: true
     },
     cache: {
