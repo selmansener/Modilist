faacccfa9c9458850d9cbf72951b7ebcadae6461
Ahmet Selman Şener <selman.sener@modilist.com>
2022-06-25 13:59:08 +0000

    Merged PR 47: Add scheduled jobs function
    Add scheduled jobs function

---
+++ src/backend/Modilist/Modilist.Functions.ScheduledJobs/Jobs/SalesOrdersDomain/CreateSalesOrdersJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Modilist.Functions.ScheduledJobs.Jobs.SalesOrdersDomain
+{
+    public class CreateSalesOrdersJob
+    {
+        [FunctionName(nameof(CreateSalesOrdersJob))]
+        public async Task CreateSalesOrdersAsync([TimerTrigger("%CreateSalesOrdersCron%")] TimerInfo timer, ILogger logger, CancellationToken cancellationToken)
+        {
+        }
+    }
+}
---
+++ src/backend/Modilist/Modilist.Functions.ScheduledJobs/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Modilist.Functions.ScheduledJobs.Startup))]
+namespace Modilist.Functions.ScheduledJobs
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
