72fd53be2ab8f84100b78f3f11a84cb24d6c94d6
Ahmet Selman Şener <selman.sener@modilist.com>
2022-08-11 17:05:50 +0000

    Merged PR 93: JsonProperty name fix

--- src/backend/services/Modilist.MailService/Modilist.MailService/Models/BeforeAccountCreatePayload.cs
+++ src/backend/services/Modilist.MailService/Modilist.MailService/Models/BeforeAccountCreatePayload.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 namespace Modilist.MailService.Models
 {
     internal class BeforeAccountCreatePayload
     {
+        [JsonProperty("client_id")]
         public string ClientId { get; set; }
 
         public string Email { get; set; }
