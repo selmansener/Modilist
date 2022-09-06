e399a9faee62f638999fc5f68bddb349a1d99697
Ezgi Gülce Yazıcı <gulce.yazici@modilist.com>
2022-09-06 09:09:11 +0000

    Merged PR 125: bug/187
    onFocus added
    
    Related work items: #187

--- src/frontend/webapp/modilist/src/pages/welcome/SizeInfo.tsx
+++ src/frontend/webapp/modilist/src/pages/welcome/SizeInfo.tsx
@@ -236,11 +236,11 @@ export function SizeInfo(props: SizeInfoProps) {
                         }}>{t("Pages.Welcome.BodySize.BodyType")}</Typography>
                     </Grid>
                     <Grid item xs={12}>
-                    <FormHelperText>
-                                <Typography variant="h3" fontWeight={800} align="center" color={theme.palette.error.main}>
-                                    {touched.bodyType && errors?.bodyType}
-                                </Typography>
-                            </FormHelperText>
+                        <FormHelperText>
+                            <Typography variant="h3" fontWeight={800} align="center" color={theme.palette.error.main}>
+                                {touched.bodyType && errors?.bodyType}
+                            </Typography>
+                        </FormHelperText>
                         <FormControl fullWidth error={touched.bodyType && errors.bodyType !== undefined}>
                             <CustomRadioButtonGroup
                                 greyscale
@@ -287,6 +287,7 @@ export function SizeInfo(props: SizeInfoProps) {
                                 helperText={touched.weight && errors.weight}
                                 onChange={handleChange}
                                 onBlur={handleBlur}
+                                onFocus={e => {e.target.select();}}
                             />
                         </FormControl>
                     </Box>
@@ -314,6 +315,7 @@ export function SizeInfo(props: SizeInfoProps) {
                                 variant="outlined"
                                 onChange={handleChange}
                                 onBlur={handleBlur}
+                                onFocus={e => {e.target.select();}}
                             />
                         </FormControl>
                     </Box>
